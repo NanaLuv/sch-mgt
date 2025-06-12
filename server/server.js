ee3c2a1d3f17a1c47a8e1b4fb3210f0009a00ecb
NanaLuv <lovenana961@gmail.com>
2025-06-12 04:14:45 +0100

    modified ports

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ mongoose
   });
 
 function startServer() {
-  const PORT = process.env.PORT || 10000;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
