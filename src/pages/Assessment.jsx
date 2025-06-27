12eb19105d8bf13cd0a20c5f0455133bbdc3a287
NanaLuv <lovenana961@gmail.com>
2025-06-27 05:16:18 +0100

    worked on api base

--- src/pages/Assessment.jsx
+++ src/pages/Assessment.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import SidebarOther from "../other-components/sidebar-other";
