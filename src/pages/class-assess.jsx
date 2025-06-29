4aecc4d398f21825796ddcd2f04cf327ab90c709
NanaLuv <lovenana961@gmail.com>
2025-06-29 02:41:31 +0100

    added api base

--- src/pages/class-assess.jsx
+++ src/pages/class-assess.jsx
@@ -23,7 +23,7 @@ const ClassAssess = () => {
   const [codeInput, setCodeInput] = useState("");
   const navigate = useNavigate();
 
-  // const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:3001";
+  const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
   useEffect(() => {
     axios
